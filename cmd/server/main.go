9b8784f00ccb134b5cf0e4ceb1ae0ac98b61aedb
RawanMostafa08 <rawanmostafa401@gmail.com>
2025-10-16 12:26:43 +0300

    feat: home page, server initialization, project structure

---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/codescalersinternships/zerodupe/pkg/server"
+	router "github.com/go-chi/chi/v5"
+)
+
+func handleRequests() {
+	r := router.NewRouter()
+
+	r.Get("/", server.HomePage)
+
+	fmt.Println("Server starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func main() {
+	handleRequests()
+}
---
+++ pkg/server/server.go
@@ -0,0 +1,10 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to zerodupe!")
+}
