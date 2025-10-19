3bb9559be743444932a72e47e7b68989640a830c
RawanMostafa08 <rawanmostafa401@gmail.com>
2025-10-19 18:02:54 +0300

    fix: client return types

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -8,45 +8,41 @@ import (
 	"os"
 )
 
-func Upload(filepath string) (*http.Response, error) {
+func Upload(filepath string) (string, error) {
 	data, err := os.ReadFile(filepath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read file: %v", err)
+		return "", fmt.Errorf("failed to read file: %v", err)
 	}
 
 	resp, err := http.Post("http://localhost:8080/upload", "application/octet-stream", bytes.NewReader(data))
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %v", err)
+		return "", fmt.Errorf("failed to send request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return resp, fmt.Errorf("failed to read response body: %v", err)
+		return string(body), fmt.Errorf("failed to read response body: %v", err)
 	}
-	fmt.Println("Response status:", resp.Status)
-	fmt.Printf("Response Body:\n%s\n", body)
 
-	return resp, nil
+	return string(body), nil
 }
 
-func Download(fileHash string) (*http.Response, error) {
+func Download(fileHash string) (string, error) {
 
 	endpoint := "http://localhost:8080/download" + "/" + fileHash
 
 	resp, err := http.Get(endpoint)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %v", err)
+		return "", fmt.Errorf("failed to send request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return resp, fmt.Errorf("failed to read response body: %v", err)
+		return string(body), fmt.Errorf("failed to read response body: %v", err)
 	}
 
-	fmt.Println("Response status:", resp.Status)
-	fmt.Printf("Response Body:\n%s\n", body)
 
-	return resp, nil
+	return string(body), nil
 }
